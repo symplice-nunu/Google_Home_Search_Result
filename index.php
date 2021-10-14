5a394792e1b88daf26066e9d833df2aad3962488
symplice-nunu <intwarisymplice@gmail.com>
2021-10-14 15:26:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Google</title>
+    <link rel="stylesheet" href="style.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  </head>
+  <body>
+    <div class="wrapper">
+        <h1 class="google" align="center">G<strong class="ored">o</strong><strong class="oyellow">o</strong>g<strong class="lgreen">l</strong><strong class="ored">e</strong>
+        </h1>
+      <div class="search-input">
+          <div class="icon"><i class="fas fa-search"></i></div>
+        <a href="" target="_blank" hidden></a>
+        <input type="text" placeholder="Type to search..">
+        <div class="autocom-box">
+          <!-- here list are inserted from javascript -->
+        </div>
+        <div class="icons"><i class="fas fa-microphone"></i></div>
+      </div>
+      <div class="btn" align="center">
+          
+      <button class="icon"> <a href="" style="text-decoration: none; color: #000;">Google Search</a></button> &nbsp; <button> <a href="https://www.google.com/doodles" style="text-decoration: none; color: #000;">I'm Feeling Lucky</a></button>
+      </div>
+      <div class="btn" align="center">
+          
+      <p>Google offered in: <span style="color: blue;">Français</span> <span style="color: blue;">Kiswahili</span> <span style="color: blue;">Ikinyarwanda</span></p>
+      </div>
+    </div>
+
+     <script src="suggestions.js"></script> 
+     <script src="script.js"></script> 
+
+  </body>
+</html>
